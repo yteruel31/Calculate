3ec556bdf6a3353f22e21fdd2e19519d2e8a0886
yteruel <yoann.teruel@viacesi.fr>
2018-11-20 12:29:27 +0100

    Using Library CalculateLib

--- CalculateMain/Program.cs
+++ CalculateMain/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using CalculateLib;
 
 namespace CalculateMain
 {
@@ -10,6 +12,7 @@ namespace CalculateMain
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
