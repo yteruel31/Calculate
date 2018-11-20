72dc06ab56e0e87b45f51de3e444f1f08e898933
yteruel <yoann.teruel@viacesi.fr>
2018-11-20 16:13:04 +0100

    Ajout de la class LogicTree

---
+++ CalculateMain/CalculateLib/LogicTree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateLib
+{
+    public class LogicTree
+    {
+        public char Operator { get; set; }
+        public decimal Number { get; set; }
+        public int Priority { get; set; }
+        public LogicTree LeftOperand { get; set; }
+        public LogicTree RightOperand { get; set; }
+
+        private static LogicTree[] BuildLogicalTree(String[] parseInput)
+        {
+            LogicTree[] logicTree = new LogicTree[parseInput.Length];
+            for (int i = 0; i < parseInput.Length; i++)
+                logicTree[i] = new LogicTree();
+
+            for (int j = 0; j < parseInput.Length; j++)
+            {
+                if (parseInput[j].Equals('*') || parseInput[j].Equals('/') || parseInput[j].Equals('+') ||
+                    parseInput[j].Equals('-'))
+                {
+                    logicTree[j].Operator = Char.Parse(parseInput[j]);
+                }
+                else
+                {
+                    logicTree[j].Number = Decimal.Parse(parseInput[j]);
+                }
+            }
+            return logicTree;
+        }
+    }
+}
