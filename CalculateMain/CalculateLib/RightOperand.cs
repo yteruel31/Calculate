6cd33a77d548b19bcf1c98462e5bf54f44a61917
yteruel <yoann.teruel@viacesi.fr>
2018-11-20 16:29:48 +0100

    Ajout de traitement dans la method MakeLogical()

--- CalculateMain/CalculateLib/RightOperand.cs
+++ CalculateMain/CalculateLib/RightOperand.cs
@@ -10,7 +10,39 @@ namespace CalculateLib
     {
         public LogicTree[] MakeLogical(LogicTree[] logicalTree)
         {
-            throw new NotImplementedException();
+            for (int i = 1; i < logicalTree.Length; i++)
+            {
+                if (logicalTree[i].Operator == '+' || logicalTree[i].Operator == '-')
+                {
+                    if (i + 2 < logicalTree.Length)
+                    {
+                        if (logicalTree[i + 2].Operator == '*' || logicalTree[i + 2].Operator == '/')
+                        {
+                            int j;
+                            for (j = i + 2; j < logicalTree.Length; j++)
+                            {
+                                if (logicalTree[j].Operator != '*' && logicalTree[j].Operator != '/')
+                                {
+                                    logicalTree[i].RightOperand = logicalTree[j - 2];
+                                    break;
+                                }
+                                j++;
+                            }
+
+                            if (logicalTree[i].RightOperand == null)
+                            {
+                                logicalTree[i].RightOperand = logicalTree[j - 2];
+                            }
+                        } else {
+                            logicalTree[i].RightOperand = logicalTree[i + 1];
+                        }
+                    } else {
+                        logicalTree[i].RightOperand = logicalTree[i + 1];
+                    }
+                }
+                i++;
+            }
+            return logicalTree;
         }
     }
 }
