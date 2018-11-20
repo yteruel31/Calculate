9a8251af2261fffff6d103a7e838db70fe97d6d1
yteruel <yoann.teruel@viacesi.fr>
2018-11-20 16:13:56 +0100

    Ajout de la class RightOperand

---
+++ CalculateMain/CalculateLib/RightOperand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateLib
+{
+    class RightOperand : IOperand
+    {
+        public LogicTree[] MakeLogical(LogicTree[] logicalTree)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
