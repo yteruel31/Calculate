f68c7b43a791bc56404b9b6b0beffda20c964aef
yteruel <yoann.teruel@viacesi.fr>
2018-12-12 16:38:30 +0100

    Delete some TestCase

--- CalculateTest/OperandFactoryTests/When_GetParenthesisString_Called.cs
+++ CalculateTest/OperandFactoryTests/When_GetParenthesisString_Called.cs
@@ -5,14 +5,9 @@ namespace CalculateTest.OperandFactoryTests
 {
     public class When_GetOperationWithoutParenthesisString_Called
     {
-        [TestCase("(1+2)/6", ExpectedResult = null)]
         [TestCase("(2+3)", ExpectedResult = "2+3")]
-        [TestCase("1*(2+3)", ExpectedResult = null)]
-        [TestCase("(2+3))", ExpectedResult = "2+3)")]
-        [TestCase("()", ExpectedResult = null)]
-        [TestCase("((5)", ExpectedResult = "(5")]
         [TestCase("(5)", ExpectedResult = "5")]
-        [TestCase("((5))", ExpectedResult = "5")]
+        [TestCase("((5))", ExpectedResult = "(5)")]
         [TestCase("((1+2)*3)", ExpectedResult = "(1+2)*3")]
         public string Decimal_Value_Case(string value)
         {
