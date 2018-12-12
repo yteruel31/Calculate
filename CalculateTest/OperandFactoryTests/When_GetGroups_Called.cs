6ee7383cdae86087af33abfd5a2c8e6c67c11846
yteruel <yoann.teruel@viacesi.fr>
2018-12-12 16:36:57 +0100

    Create UT When_GetGroups_Called

---
+++ CalculateTest/OperandFactoryTests/When_GetGroups_Called.cs
@@ -0,0 +1,14 @@
+﻿using CalculateLib.Operands;
+using NUnit.Framework;
+
+namespace CalculateTest.OperandFactoryTests
+{
+    public class When_GetGroups_Called
+    {
+        [TestCase("((1+2)*(4+3))", ExpectedResult = new[] {"((1+2)*(4+3))", "(1+2)", "(4+3)"})]
+        public string[] Default_Case(string input)
+        {
+            return OperandFactory.GetGroups(input);
+        }
+    }
+}
