e68b739a6a4439b2982cd67b0cb2c15b1c8c5b36
yteruel <yoann.teruel@viacesi.fr>
2019-01-21 17:51:40 +0100

    Ajout de la possibilité de pouvoir écrire les formules directement dans la textbox

---
+++ Calculate.WPF/Utility/NumericTextBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Calculate.WPF.Utility
+{
+    public class NumericTextBox : TextBox
+    {
+        private static readonly Regex regex = new Regex("^[0-9.()+\\-*/]+$");
+
+        protected override void OnPreviewTextInput(TextCompositionEventArgs e)
+        {
+            if (!regex.IsMatch(e.Text))
+                e.Handled = true;
+            base.OnPreviewTextInput(e);
+        }
+    }
+}
