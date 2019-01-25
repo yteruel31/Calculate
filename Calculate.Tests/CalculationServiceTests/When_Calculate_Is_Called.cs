24d23c695b60c6f456ff0205ead815230d2eb1e6
yteruel <yoann.teruel@viacesi.fr>
2019-01-25 11:02:51 +0100

    TU > CalculationService.Calculate()

--- Calculate.Tests/CalculationServiceTests/When_Calculate_Is_Called.cs
+++ Calculate.Tests/CalculationServiceTests/When_Calculate_Is_Called.cs
@@ -1,19 +1,45 @@
-﻿using Calculate.Lib.Operands;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Calculate.Lib.Operands;
 using Calculate.Lib.Services;
 using Moq;
 using NUnit.Framework;
 
 namespace Calculate.Tests.CalculationServiceTests
 {
-    [TestFixture]
     public class When_Calculate_Is_Called
     {
-        private CalculationService _calculationService;
+
+        private CalculationService GetSut()
+        {
+
+            return new CalculationService();
+        }
+
         [Test]
         public void Default_Case()
         {
-            _calculationService = new CalculationService();
-            Assert.AreEqual(45, _calculationService.Calculate(OperandFactory.Create("42+3")));
+            CalculationService calculationService = new CalculationService();
+            Assert.AreEqual(45, calculationService.Calculate(OperandFactory.Create("42+3")));
+        }
+
+        [TestCase(OperandType.Addition, ExpectedResult = 12)]
+        [TestCase(OperandType.Divide, ExpectedResult = 3)]
+        [TestCase(OperandType.Multiply, ExpectedResult = 27)]
+        [TestCase(OperandType.Substract, ExpectedResult = 6)]
+        public int When_Type_Called(OperandType type)
+        {
+            // Arrange
+            OperandFunctionBase operandFunctionBase = new OperandFunctionBase(type)
+            {
+                LeftOperand = new OperandValue(9),
+                RightOperand = new OperandValue(3)
+            };
+            CalculationService sut = GetSut();
+            // Act
+            return (int)sut.Calculate(operandFunctionBase);
         }
     }
+    
 }
