accf6806b6a53ba6056e8fcb100209b74461e022
yteruel <yoann.teruel@viacesi.fr>
2019-01-25 10:59:56 +0100

    Refactoring ObservableCollection()
    Optimistation du processus d'appel de la méthode

--- Calculate.WPF/Extensions/ListExtension.cs
+++ Calculate.WPF/Extensions/ListExtension.cs
@@ -7,10 +7,7 @@ namespace Calculate.WPF.Extensions
     {
         public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> coll)
         {
-            var c = new ObservableCollection<T>();
-            foreach (var e in coll)
-                c.Add(e);
-            return c;
+            return new ObservableCollection<T>(coll);
         }
     }
 }
