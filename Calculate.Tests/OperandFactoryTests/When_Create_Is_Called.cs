dc57f5491bead4ef0bd5a96015eb0c559873d75b
yteruel <yoann.teruel@viacesi.fr>
2019-01-30 15:06:36 +0100

    Modification du comportement du TU

--- Calculate.Tests/OperandFactoryTests/When_Create_Is_Called.cs
+++ Calculate.Tests/OperandFactoryTests/When_Create_Is_Called.cs
@@ -11,7 +11,7 @@ namespace Calculate.Tests.OperandFactoryTests
         [Test]
         public void Addition_Case()
         {
-            OperandFunctionBase operand = OperandFactory.Create("42+12");
+            OperandBase operand = OperandFactory.Create("42+12");
 
             AssertIsOperand(operand, 42, 12);
         }
@@ -19,10 +19,10 @@ namespace Calculate.Tests.OperandFactoryTests
         [Test]
         public void Addition_Three_Operand_Case()
         {
-            OperandFunctionBase operand = OperandFactory.Create("42+12+3");
+            OperandBase operand = OperandFactory.Create("42+12+3");
 
-            AssertIsOperandValue(operand.LeftOperand, 42m);
-            AssertIsOperand(operand.RightOperand, 12, 3);
+            AssertIsOperandValue(((OperandFunctionBase)operand).LeftOperand, 42m);
+            AssertIsOperand(((OperandFunctionBase)operand).RightOperand, 12, 3);
         }
 
         [Test]
@@ -34,7 +34,7 @@ namespace Calculate.Tests.OperandFactoryTests
         [Test]
         public void Divide_Case()
         {
-            OperandFunctionBase operand = OperandFactory.Create("42/12");
+            OperandBase operand = OperandFactory.Create("42/12");
 
             AssertIsOperand(operand, 42, 12);
         }
@@ -42,7 +42,7 @@ namespace Calculate.Tests.OperandFactoryTests
         [Test]
         public void Multiply_Case()
         {
-            OperandFunctionBase operand = OperandFactory.Create("42*12");
+            OperandBase operand = OperandFactory.Create("42*12");
 
             AssertIsOperand(operand, 42, 12);
         }
@@ -50,16 +50,16 @@ namespace Calculate.Tests.OperandFactoryTests
         [Test]
         public void Parenthesis_Priority_Case()
         {
-            OperandFunctionBase operand = OperandFactory.Create("(1+2)*42");
+            OperandBase operand = OperandFactory.Create("(1+2)*42");
 
-            AssertIsOperandValue(operand.RightOperand, 42);
-            AssertIsOperand(operand.LeftOperand, 1, 2);
+            AssertIsOperandValue(((OperandFunctionBase)operand).RightOperand, 42);
+            AssertIsOperand(((OperandFunctionBase)operand).LeftOperand, 1, 2);
         }
 
         [Test]
         public void Parenthesis_Unuseful_Case()
         {
-            OperandFunctionBase operand = OperandFactory.Create("(1+2)");
+            OperandBase operand = OperandFactory.Create("(1+2)");
 
             AssertIsOperand(operand, 1, 2);
         }
@@ -67,21 +67,22 @@ namespace Calculate.Tests.OperandFactoryTests
         [Test]
         public void Substract_Case()
         {
-            OperandFunctionBase operand = OperandFactory.Create("42-12");
+            OperandBase operand = OperandFactory.Create("42-12");
 
             AssertIsOperand(operand, 42, 12);
         }
 
-        private static void AssertIsOperand(OperandFunctionBase operand, decimal left, decimal right)
-        {
-            AssertIsOperandValue(operand.LeftOperand, left);
-            AssertIsOperandValue(operand.RightOperand, right);
-        }
-
-        private static void AssertIsOperandValue(OperandFunctionBase operand, decimal value)
+        private static void AssertIsOperand(OperandBase operand, decimal left, decimal right)
         {
             Assert.IsAssignableFrom<OperandFunctionBase>(operand);
-            Assert.AreEqual(value, operand.Value);
+            AssertIsOperandValue(((OperandFunctionBase)operand).LeftOperand, left);
+            AssertIsOperandValue(((OperandFunctionBase)operand).RightOperand, right);
+        }
+
+        private static void AssertIsOperandValue(OperandBase operand, decimal value)
+        {
+            Assert.IsAssignableFrom<OperandValue>(operand);
+            Assert.AreEqual(value, ((OperandValue)operand).Value);
         }
     }
 }
