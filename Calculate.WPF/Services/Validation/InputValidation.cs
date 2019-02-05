d590482bbe84ea7fc10a6f6eae27bbc18cf5fda2
Yoann TERUEL <yoann.teruel@viacesi.fr>
2019-02-05 15:56:47 +0100

    Ajout de la methode IsEndWithNumber

--- Calculate.WPF/Services/Validation/InputValidation.cs
+++ Calculate.WPF/Services/Validation/InputValidation.cs
@@ -11,5 +11,18 @@ namespace Calculate.WPF.Services.Validation
                    textInput.EndsWith(OperationModel.Multiply.Value) ||
                    textInput.EndsWith(OperationModel.Divide.Value);
         }
+        public static bool IsEndWithNumber(string textInput)
+        {
+            return textInput.EndsWith("9") ||
+                   textInput.EndsWith("8") ||
+                   textInput.EndsWith("7") ||
+                   textInput.EndsWith("6") ||
+                   textInput.EndsWith("5") ||
+                   textInput.EndsWith("4") ||
+                   textInput.EndsWith("3") ||
+                   textInput.EndsWith("2") ||
+                   textInput.EndsWith("1") ||
+                   textInput.EndsWith("0");
+        }
     }
 }
