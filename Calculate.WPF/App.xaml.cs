9e0d15947463d3818aee2b16551643c845b858e9
Yoann TERUEL <yoann.teruel@viacesi.fr>
2019-02-06 14:17:33 +0100

    Implémentation de Squirrel

--- Calculate.WPF/App.xaml.cs
+++ Calculate.WPF/App.xaml.cs
@@ -5,13 +5,27 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Squirrel;
 
 namespace Calculate.WPF
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            CheckForUpdate();
+        }
+
+        private async Task CheckForUpdate()
+        {
+            using (var manager = new UpdateManager(@"C:\Temp\Releases"))
+            {
+                await manager.UpdateApp();
+            }
+        }
     }
 }
